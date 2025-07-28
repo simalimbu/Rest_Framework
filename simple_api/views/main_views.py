92f11a431289dc5428f37803770142695cf28059
simalimbu <simalimbu2060@gmail.com>
2025-07-28 20:26:38 +0530

    Category by id

--- simple_api/views/main_views.py
+++ simple_api/views/main_views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from ..models import Category,Product
 
 #function-based
+#For category
 @api_view(['POST','GET'])
 @permission_classes([IsAuthenticated])
 def category_view(request):
@@ -22,3 +23,34 @@ def category_view(request):
         category = Category.objects.all()
         serializer =  CategorySerializer(category, many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+#For category by id
+@api_view(['PUT','GET','DELETE'])
+@permission_classes([IsAuthenticated])
+def categoryby_id(request,id):
+    
+    try:
+        category = Category.objects.get(id=id)
+    except Category.DoesNotExist:
+        return Response({'msg':"Category does not exist"})
+    
+    if request.method == 'PUT':
+        serializer = CategorySerializer(category, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':"Category updated successfully!!"},status=status.HTTP_200_OK)
+        else:
+            return Response({'msg':"Failed to update Category!!"},status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+       category = Category.objects.get(id=id)
+       category.delete()
+       return Response({'msg':"Blog deleted successfully!"},status=status.HTTP_400_BAD_REQUEST)
+   
+    elif request.method == 'GET':
+        category = Category.objects.get(id=id)
+        serializer = CategorySerializer(category)
+        return Response({'data':serializer.data},status=status.HTTP_200_OK)
+    
+    else:
+         return Response({'msg':"invalid request"},status=status.HTTP_400_BAD_REQUEST)
+     
